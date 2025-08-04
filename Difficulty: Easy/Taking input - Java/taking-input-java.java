0059dd6249bb5e93e310cb4722eed597381143c5
ArushiGUPTA2512 <arushi809gupta@gmail.com>
2025-08-04 22:59:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Taking input - Java/taking-input-java.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Implement Scanner Class
+        String s=" ";
+        Scanner in=new Scanner(System.in);
+        // Take all inputs
+        int a=in.nextInt();
+         System.out.println(a);
+        float b= in.nextFloat();
+        System.out.println(b);
+        long c= in.nextLong();
+         System.out.println(c);
+        byte d= in.nextByte();
+         System.out.println(d);
+         in.nextLine();
+        s= in.nextLine();
+         System.out.println(s);
+        // Print all the values
+    }
+}
