a3aa347f8648fc1f80e31597fd04685e64290db6
Arushi Gupta <arushi809gupta@gmail.com>
2026-09-14 20:12:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Non-Overlapping Intervals/non-overlapping-intervals.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minRemoval(int intervals[][]) {
+        // code here
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[1], b[1]));
+        int lastEnd = intervals[0][1];
+        int count = 0;
+        for(int i=1; i<intervals.length; i++){
+            if(lastEnd > intervals[i][0])
+                count++;
+            else
+                lastEnd = intervals[i][1];
+        }
+        return count;
+    }
+}
